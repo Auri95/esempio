08831528092c3549a0f1e37d1a218072d87bb161
Auri95 <danieleaurioso@gmail.com>
2017-03-03 12:53:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello C# World!");
+            printLogo();
+        }
+        static void printLogo()
+        {
+            System.Console.WriteLine("Copyright università di Genova");
+        }
+    }
+}
